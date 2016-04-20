24094cd36a6c2d3ba6b006822e462cd7eba09e5c
Fellipe Lamoglia <fellipelamoglia@gmail.com>
2016-04-20 10:07:05 -0300

    Versiona shellscript auxiliar para importaçao do banco

---
+++ shellscript/import_dump.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+mysqldump -u root --no-create-info CPGF_2016 > data.sql
+
+#Fix table names
+sed -i 's/CPGF_FAVORECIDO/favored/g' data.sql
+sed -i 's/CPGF_EXTRATO/sources/g' data.sql
+sed -i 's/CPGF_ORGAO_SUBORDINADO/subordinated_organs/g' data.sql
+sed -i 's/CPGF_ORGAO_SUPERIOR/superior_organs/g' data.sql
+sed -i 's/CPGF_PESSOA/people/g' data.sql
+sed -i 's/CPGF_TIPO_TRANSACAO/transaction_types/g' data.sql
+sed -i 's/CPGF_TRANSACAO/transactions/g' data.sql
+sed -i 's/CPGF_UNIDADE_GESTORA/management_units/g' data.sql
+
+#fix table differences
+sed -i 's/INSERT INTO `transactions` /INSERT INTO `transactions` (`id`, `value`, `date`, `superior_organ_id`, `subordinated_organ_id`, `management_unit_id`, `source_id`, `person_id`, `favored_id`, `transaction_type_id`, `created_at`, `updated_at`) /g' data.sql
+sed -i 's/INSERT INTO `favored` /INSERT INTO `favored` (`id`, `name`, `masked_document`, `created_at`, `updated_at`) /g' data.sql
+
+#Remove locks to track import progress
+sed -i 's/^UNLOCK TABLE/-- UNLOCK TABLE/g' data.sql
+sed -i 's/^LOCK TABLE/-- LOCK TABLE/g' data.sql 
+
+#insert aditional params for created_at and updated_at
+sed -i 's/),/,NOW(),NOW()),/g' data.sql
+sed -i 's/);/,NOW(),NOW());/g' data.sql
+
+#import
+pv data.sql | mysql -u root CPGF_2016_DEV
+
+#fix transactions with null dates (protected data)
+mysql -u root -e 'UPDATE transactions SET date = (select reference from sources where sources.id = transactions.source_id) , hidden_date = true
+WHERE transactions.date is null' CPGF_2016_DEV
