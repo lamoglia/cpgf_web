cb349e0c1dfc8ce34d92febfcbcc8efecb74f9b9
Fellipe Lamoglia <fellipelamoglia@gmail.com>
2016-04-30 14:01:45 -0300

    Removing helper methods from controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,43 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  include MetaTagsHelper
+  include ChartsHelper
+
   before_action :prepare_meta_tags, if: "request.get?"
-
-  def prepare_meta_tags(options={})
-    site_name   = "CPGF"
-    title       = "CPGF"
-    description = "CPGF apresenta os dados fornecidos pelo governo sobre o Cartão de pagamento do governo federal (CPGF) em um formato legível que facilita consultas e análises dos gastos declarados."
-    image       = options[:image] || "your-default-image-url"
-    current_url = request.url
-    
-    defaults = {
-      site:        site_name,
-      title:       title,
-      image:       image,
-      description: description,
-      keywords:    %w[CPGF 'Cartão de pagamento do governo federal', 'Cartão corporativo', 'dados abertos'],
-      twitter: {
-        site_name: site_name,
-        site: '@aaaaaa',
-        card: 'summary',
-        description: description,
-        image: image
-      },
-      og: {
-        url: current_url,
-        site_name: site_name,
-        title: title,
-        image: image,
-        description: description,
-        type: 'website',
-        locale: 'pt_BR'
-      }
-    }
-
-    options.reverse_merge!(defaults)
-
-    set_meta_tags options
-  end
 
   def index
     @yearly_total_chart = build_yearly_total_chart();
@@ -52,50 +19,4 @@ class ApplicationController < ActionController::Base
   def about
   end
 
-  private
-
-    def build_yearly_total_chart()
-      yearly_total = Transaction.group_by_year(:date, dates: true, format:"%Y").sum(:value)
-      yearly_total.delete_if { |key, value| key.to_i < 2009 }
-
-      LazyHighCharts::HighChart.new('graph') do |f|
-        f.xAxis(categories: yearly_total.keys)
-        f.yAxis(min: 0, title: nil)
-        f.series(name: "Gasto total", data: yearly_total.values.collect{ |a| a.to_f})
-
-        f.legend(verticalAlign: 'bottom', horizontalAlign: 'right', layout: 'horizontal')
-        f.chart({type: "column", height: 300})
-      end
-    end
-
-    def build_total_by_superior_organ_chart()
-      superior_organs_total = Array.new
-      sum = 0
-
-      total = Transaction.sum('value');
-      Transaction.select("superior_organ_id, sum(value) as total").group("superior_organ_id").order("total DESC").limit(10).each do | rec |
-        superior_organ = SuperiorOrgan.find(rec.superior_organ_id)
-        superior_organs_total << {name: superior_organ.name,data: [rec.total.to_f]}
-        sum += rec.total.to_f
-
-        if (sum > total/1.1) then
-          superior_organs_total << {name: 'OUTROS',data: [(total - sum).to_f ]}
-          break
-        end
-      end
-
-      LazyHighCharts::HighChart.new('bar') do |f|
-        f.title(text: "Gasto por Órgão Superior")
-        f.options[:plotOptions][:series] = { :stacking => 'normal' }
-        f.xAxis(categories: ["Gasto"])
-
-        superior_organs_total.each do |so|
-          f.series(so)
-        end
-
-        f.legend(reversed: true, verticalAlign: 'bottom', horizontalAlign: 'center', layout: 'horizontal')
-        f.chart({type: "bar"})
-        f.yAxis(title: {text: "Gasto Total"})
-      end
-    end
 end
---
+++ app/helpers/charts_helper.rb
@@ -0,0 +1,47 @@
+module ChartsHelper
+
+  def build_yearly_total_chart()
+      yearly_total = Transaction.group_by_year(:date, dates: true, format:"%Y").sum(:value)
+      yearly_total.delete_if { |key, value| key.to_i < 2009 }
+
+      LazyHighCharts::HighChart.new('graph') do |f|
+        f.xAxis(categories: yearly_total.keys)
+        f.yAxis(min: 0, title: nil)
+        f.series(name: "Gasto total", data: yearly_total.values.collect{ |a| a.to_f})
+
+        f.legend(verticalAlign: 'bottom', horizontalAlign: 'right', layout: 'horizontal')
+        f.chart({type: "column", height: 300})
+      end
+    end
+
+    def build_total_by_superior_organ_chart()
+      superior_organs_total = Array.new
+      sum = 0
+
+      total = Transaction.sum('value');
+      Transaction.select("superior_organ_id, sum(value) as total").group("superior_organ_id").order("total DESC").limit(10).each do | rec |
+        superior_organ = SuperiorOrgan.find(rec.superior_organ_id)
+        superior_organs_total << {name: superior_organ.name,data: [rec.total.to_f]}
+        sum += rec.total.to_f
+
+        if (sum > total/1.1) then
+          superior_organs_total << {name: 'OUTROS',data: [(total - sum).to_f ]}
+          break
+        end
+      end
+
+      LazyHighCharts::HighChart.new('bar') do |f|
+        f.title(text: "Gasto por Órgão Superior")
+        f.options[:plotOptions][:series] = { :stacking => 'normal' }
+        f.xAxis(categories: ["Gasto"])
+
+        superior_organs_total.each do |so|
+          f.series(so)
+        end
+
+        f.legend(reversed: true, verticalAlign: 'bottom', horizontalAlign: 'center', layout: 'horizontal')
+        f.chart({type: "bar"})
+        f.yAxis(title: {text: "Gasto Total"})
+      end
+    end
+end
