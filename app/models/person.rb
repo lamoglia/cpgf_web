f3b186980f7d8418aa9ccfe98e530043177060b7
Fellipe Lamoglia <fellipelamoglia@gmail.com>
2016-05-04 13:13:52 -0300

    Corrige busca por unidades e orgaos

--- app/models/person.rb
+++ app/models/person.rb
@@ -12,17 +12,17 @@ class Person < ActiveRecord::Base
   end
 
   def get_subordinated_organ
-    subordinated_organs_ids = transactions.pluck('DISTINCT subordinated_organ_id')
-    return SubordinatedOrgan.find(subordinated_organs_ids).collect(&:name).sort()
+    subordinated_organs_ids = transactions.pluck(:subordinated_organ_id).uniq
+    return SubordinatedOrgan.where(id: subordinated_organs_ids).collect(&:name).sort()
   end
 
   def get_management_unit
-    management_units_ids = transactions.pluck('DISTINCT management_unit_id')
-    return ManagementUnit.find(management_units_ids).collect(&:name).sort()
+    management_units_ids = transactions.pluck(:management_unit_id).uniq
+    return ManagementUnit.where(id: management_units_ids).collect(&:name).sort()
   end
 
   def get_superior_organ
-    superior_organs_ids = transactions.pluck('DISTINCT superior_organ_id')
-    return SuperiorOrgan.find(superior_organs_ids).collect(&:name).sort()
+    superior_organs_ids = transactions.pluck(:superior_organ_id).uniq
+    return SuperiorOrgan.where(id: superior_organs_ids).collect(&:name).sort()
   end
 end
