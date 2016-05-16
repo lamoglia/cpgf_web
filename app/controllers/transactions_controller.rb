9831167f19ed5ed198525a8532a586e0ccf2f159
Fellipe Lamoglia <fellipelamoglia@gmail.com>
2016-05-16 20:21:53 -0300

    Remove caching da pagina de transação

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,6 @@
 
 class TransactionsController < ApplicationController
   caches_action :index, :cache_path => Proc.new { |c| c.params }
-  caches_page :view
   
   def index
     @transactions = Transaction.paginate(:page => params[:page], :per_page => 15).order(date: :desc)
