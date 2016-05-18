792ea8fffd7d95c267341cb21811680834992097
Fellipe Lamoglia <fellipelamoglia@gmail.com>
2016-05-18 17:15:56 -0300

    Inclui título default

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,7 +1,7 @@
 module MetaTagsHelper
     def prepare_meta_tags(options={})
     site_name   = "Cartão de Pagamento do Governo Federal"
-    title       = ""
+    title       = "CPGF"
     description = "Contribua com a auditoria dos gastos feitos com o cartão corporativo, o cartão de pagamento do governo federal (CPGF)."
     image       = options[:image] || "http://#{request.host}#{ActionController::Base.helpers.image_path('og_image.jpg')}"
     current_url = request.url
